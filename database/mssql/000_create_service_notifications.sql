e023f76231e1ee423a81477a78930b5af1a4b319
Simon Bull <simon.bull.84@googlemail.com>
2018-12-13 11:58:30 +0000

    Add role_state table

--- database/mssql/000_create_service_notifications.sql
+++ database/mssql/000_create_service_notifications.sql
@@ -17,3 +17,17 @@ IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'user_s
     )
   END
 GO
+
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'role_state' AND TABLE_SCHEMA = 'service_notifications')
+  BEGIN
+    CREATE TABLE service_notifications.role_state (
+      service_id uniqueidentifier NOT NULL,
+      role_id uniqueidentifier NOT NULL,
+      organisation_id int NOT NULL,
+      last_action_sent varchar(50) NULL,
+      createdAt datetime2 NOT NULL,
+      updatedAt datetime2 NOT NULL,
+      CONSTRAINT [PK_ServiceNotifications_RoleState] PRIMARY KEY (service_id, role_id, organisation_id)
+    )
+  END
+GO
